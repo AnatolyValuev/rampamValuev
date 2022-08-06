15e26877ad0b32b0cf24a1fe36fd3ec7bf795911
AnatolyValuev <valuevdent@yandex.ru>
2022-08-06 16:00:19 +0700

    PlayerJump, enemyAgro

--- Assets/Scripts/PatrolEnemy.cs
+++ Assets/Scripts/PatrolEnemy.cs
@@ -11,21 +11,52 @@ namespace Quest.Enemies
         [SerializeField] private NavMeshAgent navMeshAgent;
         [SerializeField] private Transform[] waypoints;
         private int index;
-       
+       // [SerializeField] private float distance;
+        [SerializeField] private Transform player;
+        private NavMeshAgent agent;
+       // private bool boolAgro;
         void Start()
         {
             navMeshAgent.SetDestination(waypoints[0].position);
-
+            agent = GetComponent<NavMeshAgent>();
         }
 
+      
        
         void Update()
         {
-            if(navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+           Patrol();
+            
+            Agro();
+        }
+
+
+        private void Patrol()
+        {
+            if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
             {
                 index = (index + 1) % waypoints.Length;
                 navMeshAgent.SetDestination(waypoints[index].position);
             }
         }
+
+        private void Agro()
+        {
+            float distance = Vector3.Distance(transform.position, player.position);
+
+
+            if (distance <= 10 )
+            {
+                agent.SetDestination(player.position);
+                //boolAgro = true;
+            }
+            //else
+           // {
+               //boolAgro = false;
+           // }
+
+            
+        }
+
     }
 }
--- Assets/Scripts/PlayerRun.cs
+++ Assets/Scripts/PlayerRun.cs
@@ -9,7 +9,7 @@ namespace Quest.Enemies
     {
         [SerializeField] private float speed = 3f;
         [SerializeField] private float runSpeed = 5f;
-        // [SerializeField] private float speedJump = 1f;
+         [SerializeField] private float speedJump = 1f;
         [SerializeField] private float angularSpeed = 100f;
 
         [SerializeField] private GameObject _mine;
@@ -21,29 +21,38 @@ namespace Quest.Enemies
         private const string Horizontal = "Horizontal";
         private const string Vertical = "Vertical";
         private const string Running = "Running";
-        private const string Jump = "Jump";
+        //private const string Jump = "Jump";
         private const string MouseX = "Mouse X";
+        //private const string MouseY = "Mouse Y";
+        private const string Ground = "Ground";
 
 
 
         [SerializeField] private GameObject fonarik;
-        [SerializeField] private int healthFonarik = 10;
+        [SerializeField] private int healthFonarik = 10000;
 
 
 
         private Vector3 direction;
         private bool isRunning;
         private Vector3 rotationDir;
+        [SerializeField] private float time;
+
+        private bool isGround; // прыжок
+        private Rigidbody rb; // прыжок
 
 
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>(); // прыжок
+        }
 
 
-        
         void Update()
         {
             direction.x = Input.GetAxis(Horizontal);
             direction.z = Input.GetAxis(Vertical);
-            direction.y = Input.GetAxis(Jump);
+           // direction.y = Input.GetAxis(Jump);
 
 
 
@@ -54,8 +63,9 @@ namespace Quest.Enemies
             transform. Rotate(rotationDir);
 
             rotationDir.y = Input.GetAxis(MouseX) * angularSpeed * Time.deltaTime;
+            //rotationDir.x = Input.GetAxis(MouseY) * angularSpeed * -Time.deltaTime;
 
-
+            
 
             if (Input.GetKey(KeyCode.Mouse0))
             {
@@ -76,9 +86,14 @@ namespace Quest.Enemies
             if (Input.GetKey(KeyCode.Mouse1))
             {
                 Instantiate(_mine, _mineSpawnPlace.position, _mineSpawnPlace.rotation);
-                
+
             }
 
+            if (Input.GetKeyDown(KeyCode.Space) && isGround)
+            {
+                rb.AddForce(speedJump * transform.up, ForceMode.Impulse); // прыжок
+            }
+           
 
 
 
@@ -97,8 +112,14 @@ namespace Quest.Enemies
             }
         }
 
-
-
+        private void OnCollisionEnter(Collision collision)
+        {
+            isGround = collision.gameObject.CompareTag(Ground);  // прыжок
+        }
+        private void OnCollisionExit(Collision other)
+        {
+            isGround = !other.gameObject.CompareTag(Ground);   // прыжок
+        }
 
     }
 
